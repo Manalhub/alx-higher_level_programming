d9affe99c68d9c74e4662be4d908e86bfe06d521
Manalhub <manalrahali24@gmail.com>
2023-09-16 12:44:04 -0700

    fixes

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,15 +13,15 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
-
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
     cur = db.cursor()
     nmeSr = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
     cur.execute(nmeSr)
 
-    states = cur.fetchall()
-    for i in states:
+    rows = cur.fetchall()
+    for i in rows:
         print(i)
-
     # Clean up process
     cur.close()
     db.close()
